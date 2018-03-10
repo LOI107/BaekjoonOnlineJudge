de9c1caaa38d5849fb992c4e8cb1e41fc738c421
Song Jong Hun <sjh_ch@naver.com>
2018-03-10 16:29:29 +0900

    no message

---
+++ 10172 Dogs(2018.03.10).cpp
@@ -0,0 +1,36 @@
+/*
+
+< 기본사항 / 내 소스 >
+- 시간 제한(1 초 / 0 MS)
+- 메모리 제한(256 MB / 1984 KB)
+- 문제를 푼 전원의 정답 비율(39.938 %)
+
+< 문제 >
+Print the ascii dog as shown below.
+
+< 입력 >
+none.
+
+< 출력 >
+Print the picture as shown below. The long horizontal line __ is comprised of two underscore characters, and the dog’s nose is a zero.
+
+< 출처 >
+High School > PLU High School Programming Contest > PLU 2014 - Novice 3번
+
+< 알고리즘 분류 >
+- 출력
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+}
