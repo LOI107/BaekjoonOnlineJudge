eaab9a11dc2a517ff9fad207861600837277fa6e
Song Jong Hun <sjh_ch@naver.com>
2018-03-10 12:08:23 +0900

    no message

---
+++ 02557 Hello World(2018.03.10).cpp
@@ -0,0 +1,28 @@
+/*
+
+< 기본사항 >
+시간 제한	메모리 제한 	제출	정답	맞은 사람	정답 비율
+1 초	    128 MB	        80591	38912	31592	    49.046%
+
+< 문제 >
+Hello World!를 출력하시오.
+
+< 입력 >
+없음
+
+< 출력 >
+Hello World!를 출력하시오.
+
+< 알고리즘 분류 >
+출력
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+}
