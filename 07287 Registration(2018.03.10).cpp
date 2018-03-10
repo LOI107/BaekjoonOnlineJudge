3bc8a329e851044031469a240f53a2e6c54e8a2b
Song Jong Hun <sjh_ch@naver.com>
2018-03-10 12:55:02 +0900

    no message

---
+++ 07287 Registration(2018.03.10).cpp
@@ -0,0 +1,38 @@
+/*
+
+< 기본사항 / 내 소스 >
+- 시간 제한(1 초 / 0 MS)
+- 메모리 제한(128 MB / 1984 KB)
+- 문제를 푼 전원의 정답 비율(50.909 %)
+
+< 문제 >
+Print out your ICPC team number and team name.
+
+< 입력 >
+No input is given for this problem.
+
+< 출력 >
+Your program is to write to standard output. Print exactly two lines. The first line should contain your team number, 
+and the second line should contain your full team name, even if your team name contains one or more blanks (a character of ASCII 32).
+
+< 출처 >
+- ACM-ICPC > Regionals > Asia > Korea > Nationwide Internet Competition > Asia Regional - Daejeon Nationalwide Internet Competition 2013 J번
+- ACM-ICPC > Regionals > Asia > Korea > Nationwide Internet Competition > Asia Regional - Daejeon Nationalwide Internet Competition 2014 H번
+- ACM-ICPC > Regionals > Asia > Korea > Nationwide Internet Competition > Asia Regional - Daejeon Nationalwide Internet Competition 2015 K번
+- ACM-ICPC > Regionals > Asia > Korea > Nationwide Internet Competition > Asia Regional - Daejeon Nationalwide Internet Competition 2016 K번
+- ACM-ICPC > Regionals > Asia > Korea > Nationwide Internet Competition > Asia Regional - Daejeon Nationalwide Internet Competition 2017 K번
+
+< 알고리즘 분류 >
+- 출력
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << 4 << endl;
+	cout << "loi107" << endl;
+}
