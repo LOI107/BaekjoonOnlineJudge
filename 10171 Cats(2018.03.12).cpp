e6629108d9a116fb8e76976a5cbce4ccba829337
Song Jong Hun <sjh_ch@naver.com>
2018-03-12 00:23:14 +0900

    no message

---
+++ 10171 Cats(2018.03.12).cpp
@@ -0,0 +1,35 @@
+/*
+
+< 기본사항 / 내 소스 >
+- 시간 제한(1 초 / 0 MS)
+- 메모리 제한(256 MB / 1984 KB)
+- 문제를 푼 전원의 정답 비율(62.964 %)
+
+< 문제 >
+Print the ascii cat as shown below.
+
+< 입력 >
+none.
+
+< 출력 >
+Print the picture as shown below. The horizontal line __ is comprised of two underscore characters.
+
+< 출처 >
+High School > PLU High School Programming Contest > PLU 2014 - Novice 2번
+
+< 알고리즘 분류 >
+- 출력
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+}
