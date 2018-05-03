9a2291f1e7327d4242627edf919dec7fd32206fa
Song Jong Hun <sjh_ch@naver.com>
2018-05-03 15:22:19 +0900

    no message

---
+++ 11931 수 정렬하기 4(2018.05.03).cpp
@@ -0,0 +1,48 @@
+/*
+
+< 기본사항 / 내 소스 >
+- 시간 제한(1 초 / 288 MS)
+- 메모리 제한(128 MB / 8260 KB)
+- 문제를 푼 전원의 정답 비율(59.459 %)
+
+< 문제 >
+N개의 수가 주어졌을 때, 이를 내림차순으로 정렬하는 프로그램을 작성하시오.
+
+< 입력 >
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 
+이 수는 절대값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
+
+< 출력 >
+첫째 줄부터 N개의 줄에 내림차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(void)
+{
+	int caseNum, num;
+	vector<int> v;
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> caseNum;
+
+	for (int i = 0; i < caseNum; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end(), greater<int>());
+
+	for (vector<int>::iterator i = v.begin(); i != v.end(); i++)
+		cout << *i << '\n';
+}
