1746438f498ed0724d6bb768224349f129817ba0
Song Jong Hun <sjh_ch@naver.com>
2018-05-08 13:02:06 +0900

    no message

---
+++ 05337 Welcome(2018.05.08).cpp
@@ -0,0 +1,34 @@
+/*
+
+< 기본사항 / 내 소스 >
+- 시간 제한(1 초 / 0 MS)
+- 메모리 제한(128 MB / 1988 KB)
+- 문제를 푼 전원의 정답 비율(59.263 %)
+
+< 문제 >
+Print out the “Welcome” sign shown below.
+
+< 입력 >
+
+< 출력 >
+Print out the picture as shown below.
+
+< 출처 >
+- High School > PLU High School Programming Contest > PLU 2013 - Novice 1번
+
+< 알고리즘 분류 >
+- 출력
+
+*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(void)
+{
+	printf(".  .   .\n");
+	printf("|  | _ | _. _ ._ _  _\n");
+	printf("|/\\|(/.|(_.(_)[ | )(/.\n");
+}
